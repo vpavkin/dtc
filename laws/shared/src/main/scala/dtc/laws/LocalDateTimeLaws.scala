3d646d11731a33395d88ba47d3d431f554ee73c5
Vladimir Pavkin <vpavkin@gmail.com>
2016-10-10 21:13:35 +0300

    Remove duplicate property helpers

--- laws/shared/src/main/scala/dtc/laws/LocalDateTimeLaws.scala
+++ laws/shared/src/main/scala/dtc/laws/LocalDateTimeLaws.scala
@@ -2,16 +2,12 @@ package dtc.laws
 
 import dtc.LocalDateTimeTC
 import org.scalacheck.Prop
-import org.scalacheck.Prop.{False, Proof, Result}
 
 /**
   * Laws, that must be obeyed by any LocalDateTimeTC
   */
 trait LocalDateTimeLaws[A] {
   implicit def D: LocalDateTimeTC[A]
-
-  val proved = Prop(Result(status = Proof))
-  val falsified = Prop(Result(status = False))
 
   def dateMustNotThrow(x: A): Prop = {
     D.date(x)
