d7d9d4430187b805fc361a86db39778610f8c091
Vladimir Pavkin <vpavkin@gmail.com>
2016-11-13 23:26:13 +0200

    Add more same offset periods for testing

--- tests/shared/src/test/scala/dtc/tests/package.scala
+++ tests/shared/src/test/scala/dtc/tests/package.scala
@@ -13,7 +13,10 @@ package object tests {
 
   // scalastyle:off
   val sameOffsetZonedTimePeriods: List[SameZoneOffsetPeriod] = List(
-    SameZoneOffsetPeriod(LocalDate.of(2016, 3, 27), LocalTime.of(4, 0), LocalDate.of(2016, 10, 30), LocalTime.of(3, 0), TimeZoneId("Europe/Riga"))
+    SameZoneOffsetPeriod(LocalDate.of(2016, 3, 27), LocalTime.of(4, 0), LocalDate.of(2016, 10, 30), LocalTime.of(3, 0), TimeZoneId("Europe/Riga")),
+    SameZoneOffsetPeriod(LocalDate.of(2011, 3, 27), LocalTime.of(4, 0), LocalDate.of(2014, 10, 26), LocalTime.of(0, 0), TimeZoneId("Asia/Yakutsk")),
+    SameZoneOffsetPeriod(LocalDate.of(2014, 10, 26), LocalTime.of(5, 0), LocalDate.of(2019, 1, 1), LocalTime.of(0, 0), TimeZoneId("Asia/Yakutsk")),
+    SameZoneOffsetPeriod(LocalDate.of(1951, 1, 1), LocalTime.of(0, 0), LocalDate.of(1996, 4, 7), LocalTime.of(0, 0), TimeZoneId("America/Mexico_City"))
   )
 
   val genSameOffsetPeriod = Gen.oneOf(sameOffsetZonedTimePeriods)
