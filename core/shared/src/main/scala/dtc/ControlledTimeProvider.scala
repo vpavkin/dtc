e138e268217150ba50f031743cfc76235d5802b1
Vladimir Pavkin <vpavkin@gmail.com>
2017-02-17 14:38:17 +0200

    Add value getter to ControlledTimeProvider

--- core/shared/src/main/scala/dtc/ControlledTimeProvider.scala
+++ core/shared/src/main/scala/dtc/ControlledTimeProvider.scala
@@ -7,6 +7,7 @@ class ControlledTimeProvider[T](initialTime: T)(implicit Z: Zoned[T]) extends Pr
   def currentDate(zone: TimeZoneId): LocalDate = Z.date(currentTime(zone))
   def currentTime(zone: TimeZoneId): T = Z.withZoneSameInstant(_currentTime, zone)
 
+  def value: T = _currentTime
   def update(newTime: T): Unit = _currentTime = newTime
 
   private var _currentTime: T = initialTime
