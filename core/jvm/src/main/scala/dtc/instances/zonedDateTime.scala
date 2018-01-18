6f13ff393ebe5e74e00b7e5b1e489d2dd5712595
Vladimir Pavkin <vpavkin@gmail.com>
2018-01-18 14:11:17 +0200

    Add a Zoned instance for java.time.ZonedDateTime with relaxed cross-zone equality

--- core/jvm/src/main/scala/dtc/instances/zonedDateTime.scala
+++ core/jvm/src/main/scala/dtc/instances/zonedDateTime.scala
@@ -1,64 +1,39 @@
 package dtc.instances
 
-import java.time.temporal.{ChronoField, ChronoUnit}
 import java.time._
 
 import dtc._
-import dtc.syntax.timeZone._
 
 object zonedDateTime {
-  implicit val zonedDateTimeDTC: Zoned[ZonedDateTime] =
-    new Zoned[ZonedDateTime] {
-      def capture(date: LocalDate, time: LocalTime, zone: TimeZoneId): ZonedDateTime =
-        ZonedDateTime.of(date, time.truncatedTo(ChronoUnit.MILLIS), zone.zoneId)
 
-      def withZoneSameInstant(x: ZonedDateTime, zone: TimeZoneId): ZonedDateTime = x.withZoneSameInstant(zone.zoneId)
-      def withZoneSameLocal(x: ZonedDateTime, zone: TimeZoneId): ZonedDateTime = x.withZoneSameLocal(zone.zoneId)
-      def zone(x: ZonedDateTime): TimeZoneId = TimeZoneId(x.getZone.getId)
+  implicit val zonedDateTimeWithCrossZoneEquality: Zoned[ZonedDateTime] =
+    new ZonedDateTimeInstanceWithoutOrder {
+      def compare(x: ZonedDateTime, y: ZonedDateTime): Int =
+        if (lt(x, y)) -1
+        else if (gt(x, y)) 1
+        else 0
 
-      def date(x: ZonedDateTime): LocalDate = x.toLocalDate
-      def time(x: ZonedDateTime): LocalTime = x.toLocalTime.truncatedTo(ChronoUnit.MILLIS)
+      override def eqv(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        x.isEqual(y)
 
+      override def neqv(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        !x.isEqual(y)
+
+      override def lteqv(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        x.isBefore(y) || eqv(x, y)
+
+      override def lt(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        x.isBefore(y)
+
+      override def gteqv(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        x.isAfter(y) || eqv(x, y)
+
+      override def gt(x: ZonedDateTime, y: ZonedDateTime): Boolean =
+        x.isAfter(y)
+    }
+
+  implicit val zonedDateTimeWithStrictEquality: Zoned[ZonedDateTime] =
+    new ZonedDateTimeInstanceWithoutOrder {
       def compare(x: ZonedDateTime, y: ZonedDateTime): Int = x.compareTo(y)
-
-      def plus(x: ZonedDateTime, d: Duration): ZonedDateTime = x.plus(truncateToMillis(d))
-      def minus(x: ZonedDateTime, d: Duration): ZonedDateTime = x.minus(truncateToMillis(d))
-      def plusDays(x: ZonedDateTime, days: Int): ZonedDateTime = x.plusDays(days.toLong)
-      def plusMonths(x: ZonedDateTime, months: Int): ZonedDateTime = x.plusMonths(months.toLong)
-      def plusYears(x: ZonedDateTime, years: Int): ZonedDateTime = x.plusYears(years.toLong)
-
-      def withYear(x: ZonedDateTime, year: Int): ZonedDateTime = x.withYear(year)
-      def withMonth(x: ZonedDateTime, month: Int): ZonedDateTime = x.withMonth(month)
-      def withDayOfMonth(x: ZonedDateTime, dayOfMonth: Int): ZonedDateTime = x.withDayOfMonth(dayOfMonth)
-      def withHour(x: ZonedDateTime, hour: Int): ZonedDateTime = x.withHour(hour)
-      def withMinute(x: ZonedDateTime, minute: Int): ZonedDateTime = x.withMinute(minute)
-      def withSecond(x: ZonedDateTime, second: Int): ZonedDateTime = x.withSecond(second)
-      def withMillisecond(x: ZonedDateTime, millisecond: Int): ZonedDateTime = x.withNano(millisToNanos(millisecond))
-      def withTime(x: ZonedDateTime, time: LocalTime): ZonedDateTime = ZonedDateTime.of(date(x), time, zone(x).zoneId)
-      def withDate(x: ZonedDateTime, date: LocalDate): ZonedDateTime = ZonedDateTime.of(date, time(x), zone(x).zoneId)
-
-      def offset(x: ZonedDateTime): Offset = Offset(x.getOffset.getTotalSeconds)
-
-      def dayOfWeek(x: ZonedDateTime): DayOfWeek = x.getDayOfWeek
-      def dayOfMonth(x: ZonedDateTime): Int = x.getDayOfMonth
-      def month(x: ZonedDateTime): Int = x.getMonthValue
-      def year(x: ZonedDateTime): Int = x.getYear
-      def millisecond(x: ZonedDateTime): Int = x.get(ChronoField.MILLI_OF_SECOND)
-      def second(x: ZonedDateTime): Int = x.getSecond
-      def minute(x: ZonedDateTime): Int = x.getMinute
-      def hour(x: ZonedDateTime): Int = x.getHour
-
-      def yearsUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.YEARS)
-      def monthsUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.MONTHS)
-      def daysUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.DAYS)
-      def hoursUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.HOURS)
-      def minutesUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.MINUTES)
-      def secondsUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.SECONDS)
-      def millisecondsUntil(x: ZonedDateTime, until: ZonedDateTime): Long = x.until(until, ChronoUnit.MILLIS)
-
-      def utc(x: ZonedDateTime): (LocalDate, LocalTime) = {
-        val utcTime = x.withZoneSameInstant(ZoneOffset.UTC)
-        utcTime.toLocalDate -> utcTime.toLocalTime
-      }
     }
 }
