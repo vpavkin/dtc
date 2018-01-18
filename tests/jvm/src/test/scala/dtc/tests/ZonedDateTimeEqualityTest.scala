aa040e4f8d768941cb9e5d34e8311592c8b8fe57
Vladimir Pavkin <vpavkin@gmail.com>
2018-01-18 15:32:04 +0200

    Add equality tests, and changelog and update versions in readme.

---
+++ tests/jvm/src/test/scala/dtc/tests/ZonedDateTimeEqualityTest.scala
@@ -0,0 +1,21 @@
+package dtc.tests
+
+import java.time.{Instant, ZoneId, ZonedDateTime}
+
+import org.scalatest.{FunSuite, Matchers}
+
+class ZonedDateTimeEqualityTest extends FunSuite with Matchers {
+
+  private val instant = 1431231233567L
+  private val x = ZonedDateTime.ofInstant(Instant.ofEpochMilli(instant), ZoneId.of("Canada/Pacific"))
+  private val y = ZonedDateTime.ofInstant(Instant.ofEpochMilli(instant), ZoneId.of("America/Vancouver"))
+
+  test("strict equality") {
+    dtc.instances.zonedDateTime.zonedDateTimeWithStrictEquality.eqv(x, y) shouldBe false
+  }
+
+  test("cross-zone equality") {
+    dtc.instances.zonedDateTime.zonedDateTimeWithCrossZoneEquality.eqv(x, y) shouldBe true
+  }
+}
+
