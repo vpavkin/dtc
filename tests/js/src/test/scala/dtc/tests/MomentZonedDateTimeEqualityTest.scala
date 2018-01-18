09140ef8370c2780ef99d97aca182214ac1c32a3
Vladimir Pavkin <vpavkin@gmail.com>
2018-01-18 15:32:04 +0200

    Add equality tests, and changelog and update versions in readme.
    (cherry picked from commit aa040e4)

---
+++ tests/js/src/test/scala/dtc/tests/MomentZonedDateTimeEqualityTest.scala
@@ -0,0 +1,24 @@
+package dtc.tests
+
+import dtc.TimeZoneId
+import dtc.js.MomentZonedDateTime
+import moment.Moment
+import org.scalatest.{FunSuite, Matchers}
+
+class MomentZonedDateTimeEqualityTest extends FunSuite with Matchers {
+
+  private val instant = 1431231233567.0
+  private val x = MomentZonedDateTime.of(Moment(instant).tz("Canada/Pacific"), TimeZoneId("Canada/Pacific"))
+  private val y = MomentZonedDateTime.of(Moment(instant).tz("America/Vancouver"), TimeZoneId("America/Vancouver"))
+
+  test("strict equality") {
+    import dtc.instances.moment.momentZonedWithStrictEquality
+    momentZonedWithStrictEquality.eqv(x, y) shouldBe false
+  }
+
+  test("cross-zone equality") {
+    import dtc.instances.moment.momentZonedWithCrossZoneEquality
+    momentZonedWithCrossZoneEquality.eqv(x, y) shouldBe true
+  }
+}
+
